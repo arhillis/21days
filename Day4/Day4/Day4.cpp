a8e2a25f3d47897c19a19c4772b63914929339a8
arhillis <andriellerhillis@gmail.com>
2017-05-28 11:27:02 -0500

    Added some more to day 4

---
+++ Day4/Day4/Day4.cpp
@@ -0,0 +1,17 @@
+// Day4.cpp : Defines the entry point for the console application.
+//This program is for Day 4 of "Teach Yourself C++ in 21 Days" by Jesse Libery and Bradley Jones
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a=0, b=0, x=0, y=35;
+	cout << "a: " << a << "\tb: " << b;
+	cout << "\tx: " << x << "\ty: " << y;
+	cin.get();
+    return 0;
+}
+
